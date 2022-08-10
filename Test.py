b7e940161f124ce5564b3c4ff414dee9cb7263f4
bolisettisk <siva.karteek@gmail.com>
2022-08-10 10:16:47 +0100

    ,

--- Test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-
-x = np.arange(1,10)
-y = np.mean(x)
-print(y)
-
--- sm.py
+++ sm.py
@@ -1,13 +1,24 @@
 import numpy as np
+
 def sm(x):
     s = 0
     for i in range(0, len(x)):
                  s = s+x[i]
     return s
 
-x = np.arange(0,10)
-y = sm(x)
-print(x)
-print(y)
+
+def sm_alt(x):
+    s = 0
+    for i in x:
+                 s = s+i
+    return s
+
+def sum_1(x):
+    return np.sum(x)
+    
+    
+
+
+
 
 
